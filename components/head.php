8c5387e3147db469e3c59f5ccbcd6a6dbb294fd6
Felix A. Schultz <felix.schultz@engapho.com>
2025-08-13 17:18:05 +0200

    Refactor head.php and style.css for improved asset paths and menu layout; add responsive styles in responsive.css

--- components/head.php
+++ components/head.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vandfri Urinaler sureseal dandryer dyson falcon håndtørrer puslebord | Waterless</title>
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/responsive.css">
-    <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/fav/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/fav/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/fav/favicon-16x16.png">
-    <link rel="stylesheet" href="/css/leaflet/leaflet.css"
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/icons/fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/icons/fav/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/icons/fav/favicon-16x16.png">
+    <link rel="stylesheet" href="css/leaflet/leaflet.css"
         crossorigin="" />
     <link href="
     /css/lenis/lenis.min.css
@@ -27,57 +27,59 @@
     <header class="main-header">
         <section class="header-content">
             <a href="/" class="logo-container">
-                <img class="logo" src="/assets/logos/waterless-logo-2.jpg" alt="Waterless Logo">
+                <img class="logo" src="assets/logos/waterless-logo-2.jpg" alt="Waterless Logo">
             </a>
             <nav class="main-nav">
                 <!-- Navigation items can be added here -->
-                <section class="dropdown">
-                    <button class="dropdown-toggle nav-elements">Produkter</button>
-                    <section class="dropdown-menu">
-                        <article class="dropdown-content grid cols-4">
-                            <section>
-                                <p>Desinfektion Dispenser & tilbehør</p>
-                            </section>
-                            <section>
-                                <p>Waterless Urinaler & tilbehør</p>
-                            </section>
-                            <section>
-                                <p>Rengøringsartikler til urinaler m.m.</p>
-                            </section>
-                            <section>
-                                <p>DanDryer komplette hygiejneløsninger</p>
-                            </section>
-                            <section>
+                <section class="menu">
+                    <section class="dropdown">
+                        <button class="dropdown-toggle nav-elements">Produkter</button>
+                        <section class="dropdown-menu">
+                            <article class="dropdown-content grid cols-4">
+                                <section>
+                                    <p>Desinfektion Dispenser & tilbehør</p>
+                                </section>
+                                <section>
+                                    <p>Waterless Urinaler & tilbehør</p>
+                                </section>
+                                <section>
+                                    <p>Rengøringsartikler til urinaler m.m.</p>
+                                </section>
+                                <section>
+                                    <p>DanDryer komplette hygiejneløsninger</p>
+                                </section>
+                                <section>
 
-                            </section>
-                        </article>
+                                </section>
+                            </article>
+                        </section>
                     </section>
+                    <a href="/instructions" class="nav-elements">Instruktioner</a>
+                    <a href="/business" class="nav-elements">Til din virksomhed</a>
+                    <a href="/about" class="nav-elements">Om os</a>
+                    <a href="/contact" class="nav-elements">Kontakt</a>
                 </section>
-                <a href="/instructions" class="nav-elements">Instruktioner</a>
-                <a href="/business" class="nav-elements">Til din virksomhed</a>
-                <a href="/about" class="nav-elements">Om os</a>
-                <a href="/contact" class="nav-elements">Kontakt</a>
 
                 <!-- Language selector -->
                 <section class="dropdown">
                     <button class="language-selector dropdown-toggle">
-                        <img class="language-icon" src="/assets/icons/flag-denmark.jpg" alt="Language Icon">
+                        <img class="language-icon" src="assets/icons/flag-denmark.jpg" alt="Language Icon">
                     </button>
                     <section class="dropdown-menu">
                         <article class="dropdown-content languages">
                             <a href="/en" class="language-option">
-                                <img src="/assets/icons/uk.webp" alt="English Flag" class="language-icon">
+                                <img src="assets/icons/uk.webp" alt="English Flag" class="language-icon">
                                 English
                             </a>
                             <a href="/de" class="language-option">
-                                <img src="/assets/icons/flag-germany.jpg" alt="German Flag" class="language-icon">
+                                <img src="assets/icons/flag-germany.jpg" alt="German Flag" class="language-icon">
                                 Deutsch
                             </a>
                         </article>
                     </section>
                 </section>
                 <!-- CTA -->
-                <a href=" /quote" class="cta-button --header">Make an enquiry</a>
+                <a href="/quote" class="cta-button --header">Make an enquiry</a>
             </nav>
         </section>
     </header>
