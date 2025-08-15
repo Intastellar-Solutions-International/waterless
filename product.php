320d7cd13269cea1b4ebd45399c3640b3866209d
Felix A. Schultz <fs@asasoftware.aero>
2025-08-15 13:33:32 +0200

    Enhance product image functionality; add image magnifier feature and adjust header shadow for improved aesthetics

--- product.php
+++ product.php
@@ -8,8 +8,8 @@ include_once 'components/head.php';
             <img class="compatible-items" src="assets/products/housing-h1.png" alt="Housing H1">
             <img class="compatible-items" src="assets/products/housing-h3.png" alt="Housing H3">
         </section>
-        <section>
-            <img class="product-image" src="assets/products/urinal-eco-12.png" alt="Waterless urinal Eco 100% recycled plastic">
+        <section class="img-magnifier-container">
+            <img id="product-image" class="product-image" src="assets/products/urinal-eco-12.png" alt="Waterless urinal Eco 100% recycled plastic">
         </section>
         <section class="grid cols-2">
             <article class="--stretched">
@@ -48,6 +48,83 @@ include_once 'components/head.php';
         </section>
     </section>
 </main>
+<script>
+    magnify("product-image", 3);
+
+    function magnify(imgID, zoom) {
+        var img, glass, w, h, bw;
+        img = document.getElementById(imgID);
+
+        /* Create magnifier glass: */
+        glass = document.createElement("DIV");
+        glass.setAttribute("class", "img-magnifier-glass");
+
+        /* Insert magnifier glass when mouse enters image container: */
+        img.parentElement.insertBefore(glass, img);
+
+        /* Set background properties for the magnifier glass: */
+        glass.style.backgroundImage = "url('" + img.src + "')";
+        glass.style.backgroundRepeat = "no-repeat";
+        glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+        bw = 3;
+        w = glass.offsetWidth / 2;
+        h = glass.offsetHeight / 2;
+
+        /* Execute a function when someone moves the magnifier glass over the image: */
+        glass.addEventListener("mousemove", moveMagnifier);
+        img.addEventListener("mousemove", moveMagnifier);
+
+        /*and also for touch screens:*/
+        glass.addEventListener("touchmove", moveMagnifier);
+        img.addEventListener("touchmove", moveMagnifier);
+
+        function moveMagnifier(e) {
+            var pos, x, y;
+            /* Prevent any other actions that may occur when moving over the image */
+            e.preventDefault();
+            /* Get the cursor's x and y positions: */
+            pos = getCursorPos(e);
+            x = pos.x;
+            y = pos.y;
+            /* Prevent the magnifier glass from being positioned outside the image: */
+            if (x > img.width - (w / zoom)) {
+                x = img.width - (w / zoom);
+            }
+            if (x < w / zoom) {
+                x = w / zoom;
+            }
+            if (y > img.height - (h / zoom)) {
+                y = img.height - (h / zoom);
+            }
+            if (y < h / zoom) {
+                y = h / zoom;
+            }
+            /* Set the position of the magnifier glass: */
+            glass.style.left = (x - w) + "px";
+            glass.style.top = (y - h) + "px";
+            /* Display what the magnifier glass "sees": */
+            glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+        }
+
+        function getCursorPos(e) {
+            var a, x = 0,
+                y = 0;
+            e = e || window.event;
+            /* Get the x and y positions of the image: */
+            a = img.getBoundingClientRect();
+            /* Calculate the cursor's x and y coordinates, relative to the image: */
+            x = e.pageX - a.left;
+            y = e.pageY - a.top;
+            /* Consider any page scrolling: */
+            x = x - window.pageXOffset;
+            y = y - window.pageYOffset;
+            return {
+                x: x,
+                y: y
+            };
+        }
+    }
+</script>
 <?php
 include_once 'components/footer.php';
 ?>
