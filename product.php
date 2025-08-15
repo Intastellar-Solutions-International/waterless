8e8be29c6c795b5aaaa9eda961df060208a0fc5b
Felix A. Schultz <fs@asasoftware.aero>
2025-08-15 11:50:48 +0200

    Refactor product.php to enhance layout and add product details; introduce compatible items section with styling

--- product.php
+++ product.php
@@ -1,8 +1,46 @@
 <?php
 include_once 'components/head.php';
 ?>
-<main class="main-content">
-    <h2>Waterless urinal Eco 100% recycled plastic</h2>
+<main class="main-content ppad">
+    <section class="content grid cols-3">
+        <section>
+            <h2>Compatible housing</h2>
+            <img class="compatible-items" src="assets/products/housing-h1.png" alt="Housing H1">
+            <img class="compatible-items" src="assets/products/housing-h3.png" alt="Housing H3">
+        </section>
+        <section>
+            <h1>Waterless urinal Eco 100% recycled plastic</h1>
+            <img src="assets/products/urinal-eco-12.png" alt="Waterless urinal Eco 100% recycled plastic">
+        </section>
+        <section class="grid cols-2">
+            <article>
+                <h3>Dimensions</h3>
+                <ul>
+                    <li>Height: 620 mm</li>
+                    <li>Width: 360 mm</li>
+                    <li>Depth: 370 mm</li>
+                </ul>
+            </article>
+            <article>
+                <h3>Material</h3>
+                <p>100% recycled plastic</p>
+            </article>
+            <article class="--stretched">
+                <h3>Color</h3>
+                <p>Dark Grey</p>
+                <section>
+                    <p>Plumbing no.: 61.8164.102</p>
+                    <p>Waterless no.: No 12</p>
+                </section>
+                <section>
+                    <p>The drain pipe is freely accessible. Used for floor drains or wall drains or where several urinals need to be connected in series, e.g. to replace urinal troughs.</p>
+                </section>
+            </article>
+            <article class="--stretched">
+                <h3>Technical data</h3>
+            </article>
+        </section>
+    </section>
 </main>
 <?php
 include_once 'components/footer.php';
