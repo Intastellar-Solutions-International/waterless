5a9fb8417216867edff925d29c6fed870b0f0434
Felix A. Schultz <fs@asasoftware.aero>
2025-08-15 11:05:05 +0200

    Create product.php for displaying product details with header and footer includes

---
+++ product.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'components/head.php';
+?>
+<main class="main-content">
+    <h2>Waterless urinal Eco 100% recycled plastic</h2>
+</main>
+<?php
+include_once 'components/footer.php';
+?>
