af56940c95fae3f0e653a5883fb8f55f6d482d42
Felix A. Schultz <fs@asasoftware.aero>
2025-08-22 12:57:39 +0200

    Add instruction page and update links in product listings

---
+++ instruction.php
@@ -0,0 +1,18 @@
+<?php
+$pageTitle = "Instruktioner";
+include_once 'components/head.php';
+?>
+<main class="main-content ppad">
+    <!-- Main content goes here -->
+    <section class="content">
+        <a href="instructions.php">Tilbage</a>
+    </section>
+    <section class="content">
+        <h1>SureSeal Udskiftning</h1>
+        <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1109661238?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="SureSeal WATERLESS (Dansk)"></iframe></div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+    </section>
+</main>
+<?php
+include_once 'components/footer.php';
+?>
