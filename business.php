bbb63b801dd6d98e5ac39528e78aee810963a3ed
Felix A. Schultz <fs@asasoftware.aero>
2025-08-22 10:39:25 +0200

    Add business page with product listings and images

---
+++ business.php
@@ -0,0 +1,77 @@
+<?php
+$pageTitle = "Til din virksomhed";
+include_once 'components/head.php';
+?>
+<main class="main-content ppad">
+    <!-- Main content goes here -->
+    <section class="content">
+        <h1>Til din virksomhed</h1>
+        <p>Her kan du finde informationer om vores produkter og hvordan de kan hjælpe din virksomhed med at spare vand og penge.</p>
+        <section class="grid cols-5">
+            <article class="product-item">
+                <h2 class="product-title">Facility managers</h2>
+                <img src="assets/business/d4f581998be830472988a8c4a1c593109c75d84b.png" alt="Waterless urinal Eco 100% recycled plastic">
+                <a href="product?id=eco" class="cta">See more</a>
+            </article>
+            <article>
+
+            </article>
+            <article class="product-item">
+                <h2 class="product-title">Hotels & Malls</h2>
+                <img src="assets/business/34a3e22ad7c670fa4fc9879e548df0117e6e66d3.png" alt="Porcelain urinal">
+                <a href="product?id=porcelain" class="cta">See more</a>
+            </article>
+            <article>
+
+            </article>
+            <article class="product-item">
+                <h2 class="product-title">State organizations</h2>
+                <img src="assets/business/62a2184467a5a7448dbe1d10e7ce0cec86af72aa.png" alt="Housing H1">
+                <a href="product?id=housing-h1" class="cta">See more</a>
+            </article>
+            <article>
+
+            </article>
+            <article class="product-item">
+                <h2 class="product-title">Restaurants</h2>
+                <img src="assets/business/0423660d064b33b073c92f0467db96e005773db1.png" alt="Housing H3">
+                <a href="product?id=housing-h3" class="cta">See more</a>
+            </article>
+            <article>
+
+            </article>
+            <article class="product-item">
+                <h2 class="product-title">Sustainability organizations</h2>
+                <img src="assets/business/02ae495f266454f691921a5cf777754a5c9ff651.png" alt="Siphon Velocity">
+                <a href="product?id=siphon" class="cta">See more</a>
+            </article>
+            <article>
+
+            </article>
+            <article class="product-item">
+                <h2 class="product-title">Buildings owners & developers</h2>
+                <img src="assets/business/551d0a97e3d3f8a923cf655fcf412abc514241cb.png" alt="Siphon Velocity">
+                <a href="product?id=siphon" class="cta">See more</a>
+            </article>
+            <article>
+
+            </article>
+            <article class="product-item">
+                <h2 class="product-title">Purchasing managers</h2>
+                <img src="assets/business/dc11e59eb0d0889737df43161863f15f1875ee3c.png" alt="Siphon Velocity">
+                <a href="product?id=siphon" class="cta">See more</a>
+            </article>
+            <article>
+
+            </article>
+            <article class="product-item">
+                <h2 class="product-title">Architects & Designers</h2>
+                <img src="assets/business/8ff8fd13a7ad96f0de91f2e353e4f38369a37545.png" alt="Siphon Velocity">
+                <a href="product?id=siphon" class="cta">See more</a>
+            </article>
+        </section>
+    </section>
+</main>
+<?php
+include_once 'components/footer.php';
+?>
