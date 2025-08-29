3a54910d4c190c0855fb072bfddaf12b999b3123
Felix A. Schultz <fs@asasoftware.aero>
2025-08-29 09:25:18 +0200

    Add sustainable page with background image and content

---
+++ why-sustainable.php
@@ -0,0 +1,23 @@
+<?php
+$pageTitle = "Hvorfor er det bærdygtigt?";
+include_once 'components/head.php';
+?>
+<main>
+    <article class="relative">
+        <section class="paint-bg info-container">
+            <h1>Waterless Scandinavia – Leading the Way in Sustainable Restroom Solutions</h1>
+            <p>At Waterless Scandinavia, we believe that sustainability starts with smarter choices. Traditional urinals consume vast amounts of clean drinking water daily—an unnecessary waste of one of our planet’s most precious resources. Our waterless urinals eliminate the need for flushing, setting a new standard for environmental responsibility in restroom design.</p>
+        </section>
+        <img src="assets/hero/sustainable.png">
+    </article>
+    <article class="relative">
+        <section class="paint-bg info-container">
+            <h2>Conserving Water, Protecting the Future</h2>
+            <p>Water is a critical resource, yet millions of liters are flushed away every year in conventional urinals. Our innovative Waterless technology redefines this practice, conserving water with every use. By adopting Waterless solutions, businesses and public facilities can significantly reduce their water consumption, directly contributing to global water preservation efforts.</p>
+        </section>
+        <img src="assets/hero/sustainable.png">
+    </article>
+</main>
+<?php
+include_once 'components/footer.php';
+?>
